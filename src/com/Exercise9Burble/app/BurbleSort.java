60cb851ae44d238f45d7d1e6095d061ae8de7d33
carlos-java-182 <carlos.villasenor@outlook.com>
2019-10-15 13:04:49 -0500

    Burble

---
+++ src/com/Exercise9Burble/app/BurbleSort.java
@@ -0,0 +1,63 @@
+package com.Exercise9Burble.app;
+
+import java.util.Random;
+
+public class BurbleSort {
+
+	public static void main(String[] args) {
+		
+		//Constants declaration
+		
+		final int ELEMENTS_ARRAY = 100;
+		
+		//Variable declaration
+		int temp=0;
+		
+		//Arrays
+		int[] disorderedArray = new int[ELEMENTS_ARRAY];
+		int[] orderedArray = new int[ELEMENTS_ARRAY];
+		
+		Random randomNumbers = new Random(System.nanoTime());
+
+		//Create the vector with the random values
+		
+		for (int i=0; i<ELEMENTS_ARRAY;i++)
+		{
+			disorderedArray[i] = randomNumbers.nextInt(101);
+		}
+		
+		//Display disordered vector
+		
+		for (int i=0; i<ELEMENTS_ARRAY;i++)
+		{
+			System.out.print(disorderedArray[i]+" ");
+		}
+		
+		for(int i=0; i<ELEMENTS_ARRAY; i++)
+		{for(int j=0; j<ELEMENTS_ARRAY-1; j++)	
+			
+			if(disorderedArray[j]>disorderedArray[j+1])
+			
+			{
+			temp=disorderedArray[j+1];
+			disorderedArray[j+1]=disorderedArray[j];
+			disorderedArray[j]=temp;
+		
+			}
+		
+		
+		}
+		
+		System.out.println(" ");
+		
+		//Display the disordered vector
+		
+		for(int i=0; i<ELEMENTS_ARRAY;i++)
+		{
+			System.out.print(disorderedArray[i]+" ");
+			
+		}
+	}
+			
+
+}
